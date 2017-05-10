572022776f039ee8508652d81b4891e45c3ebf55
Paul H Cassidy <qawarrior@gmail.com>
2017-05-10 08:47:33 -0700

    updated the database.Server type to support connecting and returning a session

--- database/database.go
+++ database/database.go
@@ -5,8 +5,9 @@ import "gopkg.in/mgo.v2"
 
 // Server type to start, stop, and connect mongo db server
 type Server struct {
-	path string
-	cmd  *exec.Cmd
+	path    string
+	cmd     *exec.Cmd
+	Session *mgo.Session
 }
 
 // Start invokes the mongod server daemon to make it available
@@ -29,18 +30,20 @@ func (srv *Server) Stop() error {
 }
 
 // Connect sets up a session to the database
-func (srv *Server) Connect(uri string) (*mgo.Session, error) {
+func (srv *Server) Connect(uri string) error {
 	s, err := mgo.Dial(uri)
 	if err != nil {
-		return nil, err
+		return err
 	}
-	return s, nil
+	srv.Session = s
+	return nil
 }
 
 // NewServer returns a value of the Server
 func NewServer(dirpath string) *Server {
 	return &Server{
 		dirpath,
+		nil,
 		nil,
 	}
 }
